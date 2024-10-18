1643f01604efbbcc4ebdc09d16064ffbaede90cb
keisuke yoshida <82552894+suke-toudara@users.noreply.github.com>
2024-10-18 10:38:51 +0900

    Update traject_recorder_auto.hpp

--- include/map_manager/traject_recorder_auto.hpp
+++ include/map_manager/traject_recorder_auto.hpp
@@ -6,6 +6,7 @@
 #include <geometry_msgs/msg/point.hpp>
 #include <optional>
 #include <vector>
+#include <fstream>
 
 class TrajectRecorderAuto : public rclcpp::Node
 {
@@ -16,15 +17,19 @@ private:
     void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
     double calculate_distance(const geometry_msgs::msg::Point &pos1, const geometry_msgs::msg::Point &pos2);
     void save_point(const geometry_msgs::msg::Point &position);
+    void publish_marker();
+    void write_point_to_csv(const geometry_msgs::msg::Point &point);
+
 
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
 
     std::optional<geometry_msgs::msg::Point> last_position_;
     rclcpp::Time start_time_;
     std::vector<geometry_msgs::msg::Point> saved_points_;
-
-    const double distance_threshold_ = 2.0;  // 2メートル
-    const double time_threshold_ = 60.0;     // 60秒
-};
-
+    const double distance_interval_;
+    const double sampling_time_;
+    
+    std::ofstream csv_file_;
+}
 #endif  // TRAJECT_RECORDER_AUTO_HPP
